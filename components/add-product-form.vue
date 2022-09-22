aeaa34af450e31f85f7133aa0ae24830c4685dba
Danila <Ltdfm@mail.ru>
2022-09-22 16:17:02 +0300

    добавляет валидацию формы добавления карточки товара

--- components/add-product-form.vue
+++ components/add-product-form.vue
@@ -1,36 +1,123 @@
 <template>
   <form class="add-product-form">
     <Input
-      :label="'Наименование товара'"
-      :placeholder="'Введите наименование товара'"
-      :name="'name-add-product'"
+      v-model.trim="form.inputName.value"
+      :label="form.inputName.label"
+      :placeholder="form.inputName.placeholder"
+      :name="form.inputName.name"
+      :error="form.inputName.error"
     />
 
     <Textarea
-      :label="'Описание товара'"
-      :placeholder="'Введите наименование товара'"
-      :name="'description-add-product'"
+      v-model.trim="form.textareaDesc.value"
+      :label="form.textareaDesc.label"
+      :placeholder="form.textareaDesc.placeholder"
+      :name="form.textareaDesc.name"
     />
 
     <Input
-      :label="'Ссылка на изображение товара'"
-      :placeholder="'Введите ссылку'"
-      :name="'img-url-add-product'"
+      v-model.trim="form.inputImageUrl.value"
+      :label="form.inputImageUrl.label"
+      :placeholder="form.inputImageUrl.placeholder"
+      :name="form.inputImageUrl.name"
+      :error="form.inputImageUrl.error"
     />
 
     <Input
-      :label="'Цена товара'"
-      :placeholder="'Введите цену'"
-      :name="'cost-add-product'"
+      v-model.trim="form.inputProductPrice.value"
+      :label="form.inputProductPrice.label"
+      :placeholder="form.inputProductPrice.placeholder"
+      :name="form.inputProductPrice.name"
+      :error="form.inputProductPrice.error"
     />
 
     <Button
-      class="add-product-form__submit-button"
+      class="add-product-form__add-product-button"
       :label="'Добавить товар'"
-      :disabled="false"
+      :disabled="!isFormValid"
+      type="button"
     />
   </form>
 </template>
+
+<script>
+export default {
+  name: 'AddProductForm',
+  data: () => ({
+    errors: {
+      required: 'Это обязательное поле'
+    },
+    form: {
+      inputName: {
+        value: '',
+        label: 'Наименование товара',
+        placeholder: 'Введите наименование товара',
+        name: 'name-add-product',
+        error: '',
+        touched: false
+      },
+      inputImageUrl: {
+        value: '',
+        label: 'Ссылка на изображение товара',
+        placeholder: 'Введите ссылку',
+        name: 'img-url-add-product',
+        error: '',
+        touched: false
+      },
+      inputProductPrice: {
+        value: '',
+        label: 'Цена товара',
+        placeholder: 'Введите цену',
+        name: 'cost-add-product',
+        error: '',
+        touched: false
+      },
+      textareaDesc: {
+        value: '',
+        label: 'Описание товара',
+        placeholder: 'Введите наименование товара',
+        name: 'description-add-product'
+      }
+    },
+    requestTimeout: null,
+    isFormValid: false
+  }),
+  watch: {
+    form: {
+      handler () {
+        this.requestTimeout = clearTimeout(this.requestTimeout)
+        this.requestTimeout = setTimeout(() => {
+          this.validateForm()
+        }, 300)
+      },
+      deep: true
+    }
+  },
+  methods: {
+    validateForm () {
+      const { inputName, inputImageUrl, inputProductPrice } = this.form
+
+      this.setTouchedStateOnField(inputName)
+      this.setTouchedStateOnField(inputImageUrl)
+      this.setTouchedStateOnField(inputProductPrice)
+
+      this.setRequiredErrorOnField(inputName)
+      this.setRequiredErrorOnField(inputImageUrl)
+      this.setRequiredErrorOnField(inputProductPrice)
+
+      this.isFormValid = !!(inputName.value && inputImageUrl.value && inputProductPrice.value)
+    },
+    setRequiredErrorOnField (field) {
+      field.error = !field.value && field.touched ? this.errors.required : ''
+    },
+    setTouchedStateOnField (field) {
+      if (field.value) {
+        field.touched = true
+      }
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .add-product-form {
@@ -42,7 +129,7 @@
   box-shadow: 0 20px 30px rgba(0, 0, 0, 0.04), 0 6px 10px rgba(0, 0, 0, 0.02);
   border-radius: 4px;
 
-  &__submit-button {
+  &__add-product-button {
     margin-top: 8px;
   }
 }
