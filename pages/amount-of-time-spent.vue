bc43a1c6e3db575c6f760c9659305b08639c778f
Danila <Ltdfm@mail.ru>
2022-09-23 10:31:29 +0300

    правки в меню, стили в каталоге и на странице потраченного времени

--- pages/amount-of-time-spent.vue
+++ pages/amount-of-time-spent.vue
@@ -99,7 +99,7 @@ export default {
     box-shadow: 0 20px 30px rgba(0, 0, 0, 0.04), 0 6px 10px rgba(0, 0, 0, 0.02);
     border-radius: 4px;
 
-    @media (max-width: 767px) {
+    @media (max-width: 1279px) {
       width: 100%;
     }
 
